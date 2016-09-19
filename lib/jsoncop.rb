9e7c3784ab92b6c21d6e780e4232a06659c20080
Draveness <stark.draven@gmail.com>
2016-09-19 19:30:16 +0800

    Initialize jsoncop project

---
+++ lib/jsoncop.rb
@@ -0,0 +1,5 @@
+require "jsoncop/version"
+
+module Jsoncop
+  # Your code goes here...
+end
---
+++ spec/jsoncop_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Jsoncop do
+  it 'has a version number' do
+    expect(Jsoncop::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
